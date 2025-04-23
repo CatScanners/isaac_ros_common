71babae0f748cc70a71f83ade6155233a7b04246
Jetson Dev <jetson-dev@aalto.fi>
2025-04-23 23:12:01 +0300

    More entrypoint additions for groups

--- docker/scripts/entrypoint_additions/group_add.sh
+++ docker/scripts/entrypoint_additions/group_add.sh
@@ -1,6 +1,7 @@
 # Add workspace groups here
 
 # Dockerfile.zed
-adduser ${USERNAME} zed  >/dev/null
+adduser ${USERNAME} cs_zed >/dev/null
+adduser ${USERNAME} zed >/dev/null
 # Dockefile.simulation
-adduser ${USERNAME} cs_simulation  >/dev/null
+adduser ${USERNAME} cs_simulation >/dev/null
